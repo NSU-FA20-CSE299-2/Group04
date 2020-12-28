fd40476beb7259d596096a74989adc42d2b24608
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2020-12-28 15:05:11 +0600

    learning: Added the XML #6
    Designing the XML

--- Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/loginrds/MainActivity.java
+++ Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/loginrds/MainActivity.java
@@ -1,14 +1,25 @@
 package com.mahfujshaan.cse486.loginrds;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText usernameField,passwordField;
+    private TextView status,role,method;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernameField = (EditText)findViewById(R.id.editText1);
+        passwordField = (EditText)findViewById(R.id.editText2);
+
+        status = (TextView)findViewById(R.id.textView6);
+        role = (TextView)findViewById(R.id.textView7);
+        method = (TextView)findViewById(R.id.textView9);
     }
-}
