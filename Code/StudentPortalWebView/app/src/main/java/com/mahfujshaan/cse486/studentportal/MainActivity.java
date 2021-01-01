56326abd39424a59b51fb80a8038f4c66159b982
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-01 21:10:49 +0600

    enhancement #12 WebView
    Completed the task. Conversion to WebView is now complete. Still no bugs.

--- Code/StudentPortalWebView/app/src/main/java/com/mahfujshaan/cse486/studentportal/MainActivity.java
+++ Code/StudentPortalWebView/app/src/main/java/com/mahfujshaan/cse486/studentportal/MainActivity.java
@@ -3,8 +3,11 @@ package com.mahfujshaan.cse486.studentportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
  private WebView mwebView;
@@ -16,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
         WebSettings webSettings = mwebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         mwebView.loadUrl("https://rds3.northsouth.edu/");
+        mwebView.setWebViewClient(new WebViewClient());
+    }
+    @Override
+    public void onBackPressed(){
+        if(mwebView.canGoBack()){
+            mwebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
