995ba9475a78bb297d390deaca86594f22639e46
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-08 16:12:27 +0600

    project-progress #15 MainActivity
    Coding main activity, finished xmls upto homepage

--- Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/studentportal/MainActivity.java
+++ Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/studentportal/MainActivity.java
@@ -3,12 +3,27 @@ package com.mahfujshaan.cse486.studentportal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    EditText studentID, password;
+    Button buttonLogin;
+    Button newStudent;
+    Button forgotPassword;
+    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        studentID = findViewById(R.id.student_id_box);
+        password = findViewById(R.id.password_box);
     }
 }
