6ba29386c4fff315809f20d83a8c6cc0994be8b2
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-11 04:12:03 +0600

    project-progress #16 Added Navigation Layout

--- Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/studentportal/LandingPage.java
+++ Code/LoginFunction/app/src/main/java/com/mahfujshaan/cse486/studentportal/LandingPage.java
@@ -2,13 +2,9 @@ package com.mahfujshaan.cse486.studentportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -19,7 +15,7 @@ public class LandingPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_navigation);
 //LOGOUT BUTTON FUNCTION
         //logout = findViewById(R.id.logout);
 
