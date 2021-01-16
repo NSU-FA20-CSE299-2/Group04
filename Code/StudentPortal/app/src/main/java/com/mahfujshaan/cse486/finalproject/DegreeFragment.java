53a61346fc1ae6641a68c485c688e52cbcd5bb7a
Abdullah Al Mahfuj Shaan <abdullah.mahfuj@northsouth.edu>
2021-01-16 13:32:24 +0600

    project-progress #18 Fragment Codes

--- Code/StudentPortal/app/src/main/java/com/mahfujshaan/cse486/finalproject/DegreeFragment.java
+++ Code/StudentPortal/app/src/main/java/com/mahfujshaan/cse486/finalproject/DegreeFragment.java
@@ -1,6 +1,17 @@
 package com.mahfujshaan.cse486.finalproject;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class DegreeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.degree_fragment, container, false);
+    }
 }
